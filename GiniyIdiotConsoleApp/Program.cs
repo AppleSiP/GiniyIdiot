5c6114edac0b00a011ba095c8baf5e07e4730160
AppleSiP <AppleSiP@yandex.ru>
2024-03-06 19:45:15 +0500

    Переписал проверку файлов и добавил сообщение об ошибке

--- GiniyIdiotConsoleApp/Program.cs
+++ GiniyIdiotConsoleApp/Program.cs
@@ -9,26 +9,42 @@ namespace GeniyIdiotConsoleApp
         static string[] GetQuestions()
         {
             string curFile = @"QuestionsAndAnswers.txt";
-            bool dataFile = File.Exists(curFile);
-            string[] readText = File.ReadAllLines(@"QuestionsAndAnswers.txt");//считываем из файла построчно вопросы
-            string[] questions = new string[readText.Length / 2];
-            for (int i = 0, j = 0; i < readText.Length; i = i + 2, j++)
+            bool existsDataFile = File.Exists(curFile);
+            if (existsDataFile)
             {
-                questions[j] = readText[i];
+                string[] readText = File.ReadAllLines(@"QuestionsAndAnswers.txt");//считываем из файла построчно вопросы
+                string[] questions = new string[readText.Length / 2];
+                for (int i = 0, j = 0; i < readText.Length; i = i + 2, j++)
+                {
+                    questions[j] = readText[i];
+                }
+                return questions;
             }
-            return questions;
+            else
+            {
+                string[] questions = new string[0];
+                return questions;
+            }
         }
         static int[] GetAnswers()
         {
             string curFile = @"QuestionsAndAnswers.txt";
-            bool dataFile = File.Exists(curFile);
-            string[] readText = File.ReadAllLines(@"QuestionsAndAnswers.txt");//считываем из файла построчно ответы
-            int[] answers = new int[readText.Length / 2];
-            for (int i = 1, j = 0; i < readText.Length; i = i + 2, j++)
+            bool existsDataFile = File.Exists(curFile);
+            if (existsDataFile)
             {
-                answers[j] = Convert.ToInt32(readText[i]);
+                string[] readText = File.ReadAllLines(@"QuestionsAndAnswers.txt");//считываем из файла построчно ответы
+                int[] answers = new int[readText.Length / 2];
+                for (int i = 1, j = 0; i < readText.Length; i = i + 2, j++)
+                {
+                    answers[j] = Convert.ToInt32(readText[i]);
+                }
+                return answers;
             }
-            return answers;
+            else
+            {
+                int[] answers = new int[0];
+                return answers;
+            }
         }
         static int[] Shuffle(int countQuestions) //Генерация случайного порядка для вопросов
         {
@@ -51,14 +67,22 @@ namespace GeniyIdiotConsoleApp
         static string[] GetDiagnose()
         {
             string curFile = @"QuestionsAndAnswers.txt";
-            bool dataFile = File.Exists(curFile);
-            string[] readText = File.ReadAllLines(@"Diagnose.txt");//считываем из файла построчно диагнозы
-            string[] diagnose = new string[readText.Length];
-            for (int i = 0; i < readText.Length; i++)
+            bool existsDataFile = File.Exists(curFile);
+            if (existsDataFile)
             {
-                diagnose[i] = (readText[i]);
+                string[] readText = File.ReadAllLines(@"Diagnose.txt");//считываем из файла построчно диагнозы
+                string[] diagnose = new string[readText.Length];
+                for (int i = 0; i < readText.Length; i++)
+                {
+                    diagnose[i] = (readText[i]);
+                }
+                return diagnose;
             }
-            return diagnose;
+             else
+            {
+                string[] diagnose = new string[0];
+                return diagnose;
+            }
         }
         static void Main()
         {
@@ -73,33 +97,43 @@ namespace GeniyIdiotConsoleApp
                 string[] diagnose = GetDiagnose();
                 int countQuestions = answers.Length;
                 int[] randoms = Shuffle(countQuestions);
-                Console.WriteLine("На вопрос даётся 10 сек. Если готовы нажмите клавишу ENTER.");
-                while (Console.ReadKey().Key != ConsoleKey.Enter) { }//считали клавишу и сравнили с ENTER
-                //Здесь предполагается таймер
-                for (int i = 0; i < countQuestions; i++)
+                if (answers.Length > 0 && diagnose.Length > 0) 
                 {
-                    Console.WriteLine($"Вопрос №{i + 1}");
-                    Console.WriteLine(questions[randoms[i]]);
-                    int userAnswer = Convert.ToInt32(Console.ReadLine());
-                    int rigthAnswer = answers[randoms[i]];
-                    if (userAnswer == rigthAnswer)
-                        countRigthAnswer++;
+                    Console.WriteLine("На вопрос даётся 10 сек. Если готовы нажмите клавишу ENTER.");
+                    while (Console.ReadKey().Key != ConsoleKey.Enter) { }//считали клавишу и сравнили с ENTER
+                    //Здесь предполагается таймер
+                    for (int i = 0; i < countQuestions; i++)
+                    {
+                        Console.WriteLine($"Вопрос №{i + 1}");
+                        Console.WriteLine(questions[randoms[i]]);
+                        int userAnswer = Convert.ToInt32(Console.ReadLine());
+                        int rigthAnswer = answers[randoms[i]];
+                        if (userAnswer == rigthAnswer)
+                            countRigthAnswer++;
+                    }
+                    Console.WriteLine($"Количество правильных ответов: {countRigthAnswer}");
+                    Console.WriteLine($"{userName}! Ваш диагноз: {diagnose[countRigthAnswer]}");
+                    Console.WriteLine($"{userName}! Хотите пройти тест еще раз? Введите Да или Нет");
+                    string escapeCommand = Console.ReadLine();
+                    if (escapeCommand == "Да" || escapeCommand == "Lf" || escapeCommand == "да" ||
+                        escapeCommand == "lf" || escapeCommand == "ДА" || escapeCommand == "LF")
+                        continue;
+                    else if (escapeCommand == "Нет" || escapeCommand == "Ytn" || escapeCommand == "ytn" ||
+                        escapeCommand == "нет" || escapeCommand == "YTN" || escapeCommand == "НЕТ")
+                        break;
+                    else
+                    {
+                        Console.WriteLine("Некорректный ввод!");
+                        break;
+                    }
                 }
-                Console.WriteLine($"Количество правильных ответов: {countRigthAnswer}");
-                Console.WriteLine($"{userName}! Ваш диагноз: {diagnose[countRigthAnswer]}");
-                Console.WriteLine($"{userName}! Хотите пройти тест еще раз? Введите Да или Нет");
-                string escapeCommand = Console.ReadLine();
-                if (escapeCommand == "Да" || escapeCommand == "Lf" || escapeCommand == "да" || 
-                    escapeCommand == "lf" || escapeCommand == "ДА" || escapeCommand == "LF")
-                    continue;
-                else if (escapeCommand == "Нет" || escapeCommand == "Ytn" || escapeCommand == "ytn" || 
-                    escapeCommand == "нет" || escapeCommand == "YTN" || escapeCommand == "НЕТ")
-                    break;
                 else
                 {
-                    Console.WriteLine("Некорректный ввод!");
+                    Console.Write("Ошибка! Файлы не найдены!");
                     break;
                 }
+                
+                
             }
         }
     }
